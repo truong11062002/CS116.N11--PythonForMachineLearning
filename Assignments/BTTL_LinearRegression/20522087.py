618815cf217a05445f8d20c8d6c3f8948eaa95c0
nn_truong1106 <truong11062002@gmail.com>
2022-10-28 20:09:19 +0700

    dev: finish feature predict

--- Assignments/BTTL_LinearRegression/20522087.py
+++ Assignments/BTTL_LinearRegression/20522087.py
@@ -1,4 +1,5 @@
 from secrets import choice
+from tkinter import Label
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -9,8 +10,12 @@ from sklearn.linear_model import LinearRegression
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn import preprocessing
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+
 import numpy as np
 
 st.markdown("""
@@ -33,12 +38,6 @@ if uploaded_file is not None:
     
 # Load data and show dataset
 df = pd.read_csv('dataset/' + uploaded_file.name)
-
-# X = df.iloc[:, :-1]
-# y = df.iloc[:, -1]
-
-df_copy = df.copy()
-
 
 # Show the table data
 if st.checkbox('Show the dataset as table data'):
@@ -63,9 +62,8 @@ with col2:
 
 # list data
 list_feature = df.columns
-
+numOfFeature = len(list_feature)
 # Select features to predict
-# numOfFeature = len(list_feature)
 if st.checkbox('Show the relation between "Target" vs each variable'):
 	checked_variable = st.selectbox('Select one variable:', list_feature)
 
@@ -75,6 +73,7 @@ if st.checkbox('Show the relation between "Target" vs each variable'):
 	plt.xlabel(checked_variable)
 	plt.ylabel("y_target")
 	st.pyplot(fig)
+
 
 
 # Select the variables NOT to be used
@@ -130,11 +129,13 @@ if bool_std == 'Yes':
 # test size 
 option = st.selectbox(
 'Chọn cách chia dữ liệu?',
-('Normal', 'K Fold'))
+('Normal', 'K Fold Cross validation'))
 
 st.write('You selected:', option)
 
+
 if option == "Normal":
+    
     # Split the dataset
     left_column, right_column = st.columns(2)
     test_size = left_column.number_input('Validation dataset size (rate: 0.0 -> 1.0)', min_value = 0.0,
@@ -146,28 +147,87 @@ if option == "Normal":
     # random seed
     random_seed = right_column.number_input('Set random seed (0 -> ):', value =0, step=1, min_value=0)
 
+    X = df_std.iloc[:,: -1]
+    y = df_std.iloc[:, -1]
+
+    # One hot Encoder
+    X_new = X.copy()
+    
+    for i in range(numOfFeature):
+        if list_feature[i] == "Position":
+            X_new = X_new.drop(columns=['Position'])
+            ohe = OneHotEncoder(handle_unknown='ignore')
+            ohe_df = pd.DataFrame(ohe.fit_transform(X).astype(int).toarray())
+            X_new = pd.concat([X_new, ohe_df], axis=1)
+        if list_feature[i] == "State":
+            X_new = X_new.drop(columns=['State'])
+            ohe = OneHotEncoder(handle_unknown='ignore')
+            ohe_df = pd.DataFrame(ohe.fit_transform(X).astype(int).toarray())
+            X_new = pd.concat([X_new, ohe_df], axis=1)
+
+    st.dataframe(X_new)
+
     # split the dataset
-    X_train, X_test, Y_train, Y_test = train_test_split(df_std.iloc[:,: -1], df_std.iloc[:, -1], test_size = test_size, random_state=random_seed)
+    X_train, X_test, Y_train, Y_test = train_test_split(X_new , y, test_size = test_size, random_state=random_seed)
 
 
-if option == "K Fold":
+    # Model linear regression
+    regressor = LinearRegression()
+    regressor.fit(X_train, Y_train)
+
+    # Validation
+    Y_pred_train = regressor.predict(X_train)
+    Y_pred_test = regressor.predict(X_test)
+
+    # Using the r2 value as a validation indicator
+    r2 = r2_score(Y_test, Y_pred_test)
+    mse = mean_squared_error(Y_test, Y_pred_test, squared=False)
+    mae = mean_absolute_error(Y_test, Y_pred_test)
+    if st.button("Run"):
+        st.write(f'R2 score: {r2:.2f}')
+        st.write(f'Mean Squared Error: {mse:.2f}')
+        st.write(f'Mean Absolute Error: {mae:.2f}')
+
+
+if option == "K Fold Cross validation":
     num_folds = st.number_input("Nhập K: ", step=1, min_value = 2)
+
     # Define K-Fold Cross validation
-    k_fold = KFold(n_splits = num_folds, shuffle=True, random_state = None)
+    k_fold = KFold(n_splits = int(num_folds), shuffle=True, random_state = None)
 
     X = df_std.iloc[:,: -1]
     y = df_std.iloc[:, -1]
 
+    X_new = X.copy()
+    
+    for i in range(numOfFeature):
+        if list_feature[i] == "Position":
+            X_new = X_new.drop(columns=['Position'])
+            ohe = OneHotEncoder(handle_unknown='ignore')
+            ohe_df = pd.DataFrame(ohe.fit_transform(X).astype(int).toarray())
+            X_new = pd.concat([X_new, ohe_df], axis=1)
+        if list_feature[i] == "State":
+            X_new = X_new.drop(columns=['State'])
+            ohe = OneHotEncoder(handle_unknown='ignore')
+            ohe_df = pd.DataFrame(ohe.fit_transform(X).astype(int).toarray())
+            X_new = pd.concat([X_new, ohe_df], axis=1)
+
+    # Using Linear regression model to predict
     model = LinearRegression()
-    scores = cross_val_score(model, X, y, cv=k_fold)
+    scores = cross_val_score(model, X_new, y, cv=k_fold)
+    btn = st.button("Run")  
 
-    # st.write("Score: ", scores)
-    # st.write("Mean: ", scores.mean())
-    # st.write("Standard Deviation: ", scores.std())
-    
+    if btn:
+        st.write("Score by Cross-Validation (K-Fold): ", scores)
+        st.write("Mean: ", scores.mean())
+        st.write("Standard Deviation: ", scores.std())
+
     
 
-st.button("Run")
+
+
+
+    
 
 
 
