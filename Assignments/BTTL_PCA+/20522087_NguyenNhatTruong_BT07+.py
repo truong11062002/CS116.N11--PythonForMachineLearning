f8f3a82d1593cec0577e719704324b8d0e41d5c0
nn_truong1106 <truong11062002@gmail.com>
2022-11-23 20:45:49 +0700

    upload bt07 plus

--- Assignments/BTTL_PCA+/20522087_NguyenNhatTruong_BT07+.py
+++ Assignments/BTTL_PCA+/20522087_NguyenNhatTruong_BT07+.py
@@ -196,7 +196,6 @@ if option == "Train/Test split":
 
 if option == "K Fold Cross validation":
 
-    
     # Train/test
     X = df.iloc[:, :-1]
     y = df['target']
@@ -205,121 +204,111 @@ if option == "K Fold Cross validation":
     scaler = StandardScaler()
     x_std = scaler.fit_transform(X)
 
-
-    # -------------- PCA 
-    pca = PCA(n_components=num_pca)
-    x_pca = pca.fit_transform(x_std)
     
-
-    st.markdown("""
-    # After applied PCA
-    """)
-    st.dataframe(x_pca)
-    st.sidebar.markdown(
-    r"""
-    ### Select the principal components to plot
-    ex. Choose '1' for PCA 1
-    """
-    )
-    # Index of PCA, e.g. 1 for PCA 1, 2 for PCA 2, etc..
-    idx_x_pca = st.sidebar.selectbox("x axis is the principal component of ", np.arange(1, num_pca+1), 0)
-    idx_y_pca = st.sidebar.selectbox("y axis is the principal component of ", np.arange(1, num_pca+1), 1)
-    idx_z_pca = st.sidebar.selectbox("z axis is the principal component of ", np.arange(1, num_pca+1), 2)
-
-    # Axis label
-    x_lbl, y_lbl, z_lbl = f"PCA {idx_x_pca}", f"PCA {idx_y_pca}", f"PCA {idx_z_pca}"
-    # data to plot
-    x_plot, y_plot, z_plot = x_pca[:,idx_x_pca-1], x_pca[:,idx_y_pca-1], x_pca[:,idx_z_pca-1]
-    # Split the dataset
-
-    trace1 = go.Scatter3d(
-    x=x_plot, y=y_plot, z=z_plot,
-    mode='markers',
-    marker=dict(
-        size=5,
-        color=y,
-        )
-    )   
-
-    # Create an object for graph layout
-    fig = go.Figure(data=[trace1])
-    fig.update_layout(scene = dict(
-                        xaxis_title = x_lbl,
-                        yaxis_title = y_lbl,
-                        zaxis_title = z_lbl),
-                        width=700,
-                        margin=dict(r=20, b=10, l=10, t=10),
-                        )
-    # ----------------------------------------
-    st.plotly_chart(fig, use_container_width=True)
-    # ----------------------------------------
-
     X = X.to_numpy()
     y = y.to_numpy()
+    # create list for each dimension
+    f1_list_dimension = []
+    precision_list_dimension = []
+    recall_list_dimension = []
+    ll_list_dimension = []
 
-    # Num of folds
-    num_folds = st.sidebar.number_input("Nhập K: ", step=1, min_value = 2)
-    # random seed
-    random_seed = st.sidebar.number_input('Set random seed (0 -> ):', value =0, step=1, min_value=0)
+    arr_component = []
+    for n_component in range(1, len(dataset.feature_names) + 1):
+        
+        # Define K-Fold Cross validation
+        kf = KFold(n_splits = 5, shuffle=True, random_state = 10)
+        arr_component.append(n_component)
+        precision = []
+        recall = []
+        f1 = []
+        ll = []
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = x_std[train_index], x_std[test_index]
+            Y_train, Y_test = y[train_index], y[test_index]
 
-    # Define K-Fold Cross validation
-    kf = KFold(n_splits = int(num_folds), shuffle=True, random_state = random_seed)
+            pca = PCA(n_components=n_component)
+            pca.fit(X_train)
+            
+            X_train, X_test = pca.transform(X_train), pca.transform(X_test)
+            
+            # Model logistic regression
+            logisticRegr = LogisticRegression()
+            logisticRegr.fit(X_train, Y_train)
 
-    precision = []
-    recall = []
-    f1 = []
-    ll = []
-    for train_index, test_index in kf.split(X):
-        X_train, X_test = x_pca[train_index], x_pca[test_index]
-        Y_train, Y_test = y[train_index], y[test_index]
+            Y_pred_train = logisticRegr.predict(X_train)
+            Y_pred_test = logisticRegr.predict(X_test)
 
-        # Model logistic regression
-        logisticRegr = LogisticRegression()
-        logisticRegr.fit(X_train, Y_train)
+            # Using the r2 value as a validation indicator
+            precision.append(precision_score(Y_test, Y_pred_test, average='macro'))
+            recall.append(recall_score(Y_test, Y_pred_test, average='macro'))
+            f1.append(f1_score(Y_test, Y_pred_test, average='macro'))
+            prediction_prob = logisticRegr.predict_proba(X_test)
+            ll.append(log_loss(Y_test, prediction_prob))
 
-        Y_pred_train = logisticRegr.predict(X_train)
-        Y_pred_test = logisticRegr.predict(X_test)
+            
+            
+        
+        st.write("With component: ", n_component)
+        st.write("Mean precision: ", np.mean(precision))
+        st.write("Mean recall: ", np.mean(recall))
+        st.write("Mean f1 score: ", np.mean(f1))
+        st.write("Log loss score: ", np.mean(ll))
 
-        # Using the r2 value as a validation indicator
-        precision.append(precision_score(Y_test, Y_pred_test, average='macro'))
-        recall.append(recall_score(Y_test, Y_pred_test, average='macro'))
-        f1.append(f1_score(Y_test, Y_pred_test, average='macro'))
-        prediction_prob = logisticRegr.predict_proba(X_test)
-        ll.append(log_loss(Y_test, prediction_prob))
+        # save f1, precision, recall, ll score in each dimension
+        f1_list_dimension.append(np.mean(f1))
+        precision_list_dimension.append(np.mean(precision))
+        recall_list_dimension.append(np.mean(recall))
+        ll_list_dimension.append(np.mean(ll))
 
-    st.write("Mean precision: ", np.mean(precision))
-    st.write("Mean recall: ", np.mean(recall))
-    st.write("Mean f1 score: ", np.mean(f1))
-    st.write("Log loss score: ", np.mean(ll))
+        df_precision = pd.DataFrame(precision, columns=["Score"])
+        df_recall = pd.DataFrame(recall, columns=["Score"])
+        df_f1 = pd.DataFrame(f1, columns=["Score"])
 
-    df_precision = pd.DataFrame(precision, columns=["Score"])
-    df_recall = pd.DataFrame(recall, columns=["Score"])
-    df_f1 = pd.DataFrame(f1, columns=["Score"])
+        df_f1.index = df_f1.index.factorize()[0] + 1
+        df_recall.index = df_recall.index.factorize()[0] + 1
+        df_precision.index = df_precision.index.factorize()[0] + 1
 
-    df_f1.index = df_f1.index.factorize()[0] + 1
-    df_recall.index = df_recall.index.factorize()[0] + 1
-    df_precision.index = df_precision.index.factorize()[0] + 1
+        df_recall.reset_index(inplace=True)
+        df_recall = df_recall.rename(columns = {'index':'Fold'})
+        df_precision.reset_index(inplace=True)
+        df_precision = df_precision.rename(columns = {'index':'Fold'})
+        df_f1.reset_index(inplace=True)
+        df_f1 = df_f1.rename(columns = {'index':'Fold'})
 
-    df_recall.reset_index(inplace=True)
-    df_recall = df_recall.rename(columns = {'index':'Fold'})
-    df_precision.reset_index(inplace=True)
-    df_precision = df_precision.rename(columns = {'index':'Fold'})
-    df_f1.reset_index(inplace=True)
-    df_f1 = df_f1.rename(columns = {'index':'Fold'})
+        precision, recall, f1 = st.tabs(["Precision", "Recall", "F1"])
 
-    precision, recall, f1 = st.tabs(["Precision", "Recall", "F1"])
-
-    with precision:
-        c = alt.Chart(df_precision).mark_bar(size=30).encode(alt.X('Fold', axis=alt.Axis(title='Fold', tickMinStep=1)), y='Score')
-        st.altair_chart(c, use_container_width=False)
-    with recall:
-        c = alt.Chart(df_recall).mark_bar(size=30).encode(alt.X('Fold', axis=alt.Axis(title='Fold', tickMinStep=1)), y='Score')
-        st.altair_chart(c, use_container_width=False)
-    with f1:
-        c = alt.Chart(df_f1).mark_bar(size=30).encode(alt.X('Fold', axis=alt.Axis(title='Fold', tickMinStep=1)), y='Score')
-        st.altair_chart(c, use_container_width=False)
+        with precision:
+            c = alt.Chart(df_precision).mark_bar(size=30).encode(alt.X('Fold', axis=alt.Axis(title='Fold', tickMinStep=1)), y='Score')
+            st.altair_chart(c, use_container_width=False)
+        with recall:
+            c = alt.Chart(df_recall).mark_bar(size=30).encode(alt.X('Fold', axis=alt.Axis(title='Fold', tickMinStep=1)), y='Score')
+            st.altair_chart(c, use_container_width=False)
+        with f1:
+            c = alt.Chart(df_f1).mark_bar(size=30).encode(alt.X('Fold', axis=alt.Axis(title='Fold', tickMinStep=1)), y='Score')
+            st.altair_chart(c, use_container_width=False)
 
     
+    chart_data_f1 = pd.DataFrame(np.array(f1_list_dimension))
+    chart_data_f1.index = np.arange(1, len(chart_data_f1) + 1)
+    st.bar_chart(chart_data_f1)
+    st.write("Chiều tối ưu nhất với f1: ", np.array(f1_list_dimension).argmax()+1)
+    
+    chart_data_precision = pd.DataFrame(np.array(precision_list_dimension))
+    chart_data_precision.index = np.arange(1, len(chart_data_precision) + 1)
+    st.bar_chart(chart_data_precision)
+    st.write("Chiều tối ưu nhất với precision: ", np.array(precision_list_dimension).argmax()+1)
+
+    chart_data_recall = pd.DataFrame(np.array(recall_list_dimension))
+    chart_data_recall.index = np.arange(1, len(chart_data_recall) + 1)
+    st.bar_chart(chart_data_recall)
+    st.write("Chiều tối ưu nhất với recall: ", np.array(recall_list_dimension).argmax()+1)
+
+    chart_data_ll = pd.DataFrame(np.array(ll_list_dimension))
+    chart_data_ll.index += 1
+    st.bar_chart(chart_data_ll)
+    st.write("Chiều tối ưu nhất với log loss: ", np.array(ll_list_dimension).argmin()+1)
+
     predict_btn = st.sidebar.button('Predict', key=2)
 
 
